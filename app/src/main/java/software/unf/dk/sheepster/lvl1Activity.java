b184451300dde1b88a03417a7b8b9cc9bae4e9d9
deltager <github@software.unf.dk>
2017-07-06 13:54:32 +0200

    lvl1Activity added

---
+++ app/src/main/java/software/unf/dk/sheepster/lvl1Activity.java
@@ -0,0 +1,15 @@
+package software.unf.dk.sheepster;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class lvl1Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lvl1);
+    }
+}
