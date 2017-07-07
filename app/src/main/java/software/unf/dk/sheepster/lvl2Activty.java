af5616828df357f18680336f31be37396491efcc
deltager <github@software.unf.dk>
2017-07-07 11:58:16 +0200

    Added lvl 2 animation - not working

--- app/src/main/java/software/unf/dk/sheepster/lvl2Activty.java
+++ app/src/main/java/software/unf/dk/sheepster/lvl2Activty.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class lvl2Activty extends MainActivity {
 
     private TextView clickCount2;
+    private CanvasView lvl2Sheep;
+    private ImageView fence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class lvl2Activty extends MainActivity {
         setContentView(R.layout.lvl2);
         clickCount2 = findViewById(R.id.countlvl2);
         life = 3;
+        lvl2Sheep = (CanvasView) findViewById(R.id.lvl1Sheep);
+        fence = (ImageView) findViewById(R.id.fence);
 
     }
 
@@ -24,6 +29,7 @@ public class lvl2Activty extends MainActivity {
         count2++;
         String count3 = "" + count2;
         clickCount2.setText(count3);
+        lvl2Sheep.animation();
     }
 
     public void backButton2(View view) {
